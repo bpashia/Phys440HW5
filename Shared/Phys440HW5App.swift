108ba572951b3d43012f332f207b55883a7ea79f
bpashia <bbpashia@gmail.com>
2022-03-11 12:56:27 -0600

    Initial Commit

---
+++ Shared/Phys440HW5App.swift
@@ -0,0 +1,17 @@
+//
+//  Phys440HW5App.swift
+//  Shared
+//
+//  Created by Broc Pashia on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Phys440HW5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
